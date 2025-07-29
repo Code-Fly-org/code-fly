1b0c68aff3f28a1faafd76399c7c4f2b13278237
Lncvrt <lncvrtreal@gmail.com>
2025-07-29 16:08:06 -0700

    Add stuff to projects page

--- src/projectmanager/routes/Projects.tsx
+++ src/projectmanager/routes/Projects.tsx
@@ -1,7 +1,14 @@
+import './Projects.css'
+
 export default function Projects () {
   return (
     <>
-      <p>Projects</p>
+      <div className='topbar'>
+        <button>New Project</button>
+        <button>Open</button>
+        <button>Clone Repository</button>
+      </div>
+      <div className='content'></div>
     </>
   )
 }
