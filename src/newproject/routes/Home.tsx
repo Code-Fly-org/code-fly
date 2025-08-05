a8bf82c7dfe842bdeb569f172919f24e4619a890
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-08-05 11:38:10 -0700

    Added text boxes for the project name and project path
    Idk what other options there really needs to be but I'll try to think of
    stuff

--- src/newproject/routes/Home.tsx
+++ src/newproject/routes/Home.tsx
@@ -1,18 +1,37 @@
 import { useState } from 'react'
 import OptionButtons from '../components/OptionButtons'
+import './Home.css'
 
 export default function Home () {
   const [projectType, setProjectType] = useState<string>('website')
+  const [projectPath, setProjectPath] = useState<string>('');
+  const [projectName, setProjectName] = useState<string>('');
 
   return (
     <>
-        <OptionButtons
-          buttons={[
-            { label: 'Website', onClick: () => setProjectType('website') },
-            { label: 'Web Server', onClick: () => setProjectType('webserver') }
-          ]}
-          name="Project Type"
-        />
+        <div className='justify-center mx-auto w-fit'>
+          <input
+            type="text"
+            value={projectName}
+            onChange={(e) => setProjectName(e.target.value)}
+            placeholder="Project Name"
+          />
+
+          <input
+            type="text"
+            value={projectPath}
+            onChange={(e) => setProjectPath(e.target.value)}
+            placeholder="Project Path"
+          />
+
+          <OptionButtons
+            buttons={[
+              { label: 'Website', onClick: () => setProjectType('website') },
+              { label: 'Web Server', onClick: () => setProjectType('webserver') }
+            ]}
+            name="Project Type"
+          />
+        </div>
     </>
   )
 }
