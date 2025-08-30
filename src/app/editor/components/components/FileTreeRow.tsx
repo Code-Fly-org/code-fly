3bf5e98c245fdc04f0432c62bd14c056fab75629
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-08-29 19:04:09 -0700

    Added an icon for files without a file extension
    The icon is gonna be used for folders but for now its just for anything
    without a file extension.

--- src/app/editor/components/components/FileTreeRow.tsx
+++ src/app/editor/components/components/FileTreeRow.tsx
@@ -3,11 +3,20 @@ import './FileTreeRow.css'
 import Image from "next/image";
 
 import php_icon from "./icons/php.svg";
+import folder_icon from "./icons/folder.svg";
 
 export default function FileTreeRow({ name }: { name: string }) {
+  let file_icon = null
+
+  if (name.endsWith(".php")) {
+    file_icon = php_icon
+  } else {
+    file_icon = folder_icon
+  }
+
   return (
     <div className="file-tree-row">
-      {name.endsWith(".php") && <Image src={php_icon} alt="PHP Icon" width={50} height={50} />}
+      {file_icon != null && <Image src={file_icon} alt="File Icon" width={50} height={50} />}
       <h1>{name}</h1>
     </div>
   )
