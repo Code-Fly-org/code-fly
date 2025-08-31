157ea671409e40e512db7835fcc49bf85aa8468d
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-08-30 21:06:56 -0700

    Added an icon for txt files

--- src/app/editor/components/components/FileTreeRow.tsx
+++ src/app/editor/components/components/FileTreeRow.tsx
@@ -4,12 +4,15 @@ import Image from "next/image";
 
 import php_icon from "./icons/php.svg";
 import folder_icon from "./icons/folder.svg";
+import txt_icon from "./icons/txt.svg";
 
 export default function FileTreeRow({ name }: { name: string }) {
   let file_icon = null
 
   if (name.endsWith(".php")) {
     file_icon = php_icon
+  } else if (name.endsWith(".txt")) {
+    file_icon = txt_icon
   } else {
     file_icon = folder_icon
   }
